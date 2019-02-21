5e4dd28040c82b882a7f1083addc1e2cab48219e
Will Mackey <wfmackey@gmail.com>
2019-02-21 18:24:08 +1100

    MORE UPDATES FOR PRECIOUS TRAVIS

--- R/geog_dat.R
+++ R/geog_dat.R
@@ -2,18 +2,24 @@
 #'
 #' Geospatial data provided by the ABS for Statistical Area 1 in 2011.
 #'
-#' @format An \code{sf} object with five variables:
+#' @format An \code{sf} object with 17 variables:
 #' #' \describe{
-#' \item{\code{sa1_code}}{The full 11 digit SA1 code numeric}
-#' \item{\code{sa1_7dig}}{The 7 digit SA1 code numeric}
-#' \item{\code{sa2_code}}{The full 9 digit SA2 code numeric}
-#' \item{\code{sa2_shortcode}}{The 5 digit SA2 code numeric}
-#' \item{\code{sa2}}{The SA2 name character}
-#' \item{\code{sa3_code}}{The full 5 digit SA3 code numeric}
-#' \item{\code{sa3}}{The SA3 name character}
-#' \item{\code{sa4_code}}{The full 3 digit SA4 code numeric}
-#' \item{\code{sa4}}{The SA4 name character}
-#' \item{\code{gcc_code}}{The alphanumeric Greater Capital City (GCC) code numeric}
-#' \item{\code{gcc}}{The Greater Capital City (GCC) name}
+#' \item{\code{sa1_code_2011}}{The full 11 digit SA1 code numeric}
+#' \item{\code{sa1_7dig_2011}}{The 7 digit SA1 code numeric}
+#' \item{\code{sa2_code_2011}}{The full 9 digit SA2 code numeric}
+#' \item{\code{sa2_shortcode_2011}}{The 5 digit SA2 code numeric}
+#' \item{\code{sa2_name_2011}}{The SA2 name character}
+#' \item{\code{sa3_code_2011}}{The full 5 digit SA3 code numeric}
+#' \item{\code{sa3_name_2011}}{The SA3 name character}
+#' \item{\code{sa4_code_2011}}{The full 3 digit SA4 code numeric}
+#' \item{\code{sa4_name_2011}}{The SA4 name character}
+#' \item{\code{gcc_code_2011}}{The alphanumeric Greater Capital City (GCC) code numeric}
+#' \item{\code{state_name_2011}}{The full state name}
+#' \item{\code{albers_sqkm_2011}}{The area in square kilometres}
+#' \item{\code{cent_lat}}{The latitide of the area's centroid}
+#' \item{\code{cent_long}}{The latitide of the area's centroid}
+#' \item{\code{geometry}}{A nested list containing the area's geometry (polygons)}
 #' }
 "sa12011"
+
+names(sa12011)
