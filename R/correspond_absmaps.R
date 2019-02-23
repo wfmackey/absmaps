4fd2c5ae6f2780cfdccb835a2cb86441aa6f42b1
Will Mackey <wfmackey@gmail.com>
2019-02-23 16:38:53 +1100

    Update correspond and documentation

--- R/correspond_absmaps.R
+++ R/correspond_absmaps.R
@@ -1,11 +1,15 @@
 #' Read in ASGC/ASGS population-weighted correspondence tables from the ABS.
 #' @name correspond_absmaps
 #' @param data The original data.frame or tibble containing  that you want 'corresponded'.
+#' @param correspondFrom The variable to correspond from.
 #' @param fromArea The original area to correspond from ("sa1", "sa2", etc; see documentation for the full list.)
 #' @param fromYear The year to correspond from (2011, 2016, etc; see documentation for full list.)
 #' @param toArea The original area to correspond to ("sa1", "sa2", etc; see documentation for the full list.)
 #' @param toYear The year to correspond to (2011, 2016, etc; see documentation for full list.)
 #' @param nVar The variable you want distributed according to the correspondence ratio.
+#' @param summarise TRUE if you would like to collapse your data to be one observation per new area.
+#' If FALSE, one observation per original area will be returned.
+#' @param groupVar If you would like to group your summary by an additional variable.
 #' For example, \code{count} or \code{n} that represent the number of people in a given area.
 #'
 #'
@@ -14,7 +18,12 @@
 #' @return A \code{tibble} object.
 #' @examples
 #' \dontrun{
-#'
+#' lga %>%
+#' correspond_absmaps(correspondFrom = lga_name_2017,
+#'                    nVar = employees,
+#'                    fromArea = "lga", fromYear = 2017,
+#'                    toArea = "sa3", toYear = 2016,
+#'                    groupVar = year)
 #' }
 #' @export
 
@@ -31,7 +40,8 @@ correspond_absmaps <- function(data,
                                fromYear,
                                toArea,
                                toYear ,
-                               groupVar
+                               groupVar,
+                               summarise = TRUE
                                ) {
 
   correspondence <- get_correspondence_absmaps(
@@ -56,7 +66,7 @@ correspond_absmaps <- function(data,
 
   # Set up by(c(...)) for join
   join_cols = c(fromVarChar)
-  names(join_cols) <- correspondFrom
+  names(join_cols) <- deparse(substitute(correspondFrom))
 
   d <-
     # This will create a new row for each combination
@@ -64,12 +74,18 @@ correspond_absmaps <- function(data,
                      by = join_cols) %>%
     # Re-distribute n evenly according to pc
     dplyr::mutate(q = !!nVar * ratio) %>%
+    dplyr::filter(!is.na(!!toVar))
+
+  # Summarise if requested
+  if (summarise) {
+  d <- d %>%
     # Summarise over the 'to' variable
     dplyr::group_by(!!toVar, !!groupVar) %>%
-    dplyr::summarise(q = sum(q)) %>%
-    dplyr::filter(!is.na(!!toVar)) %>%
-    # Rename back to originals
-    dplyr::rename(!!nVar := q)
+    dplyr::summarise(q = sum(q))
+  }
+
+  # Rename back to originals
+  d <- d %>% dplyr::rename(!!nVar := q)
 
   return(d)
 
